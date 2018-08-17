ef3c2af58416bbfdba9bac0ecd6674ec336c4b41
Alex Corey <alex.swansboro@gmail.com>
2018-08-17 19:32:31 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,43 +1,89 @@
-
 const canvas = document.getElementById('screen')
-const screen = canvas.getContext('2d')
-const screenSize = {x: canvas.width, y: canvas.height} 
-
-class FieldOfPlay {
-    constructor(screenSize, screen, walls){
+const context = canvas.getContext('2d')
+const screenSize = {
+    x: canvas.width,
+    y: canvas.height
+}
+class Game {
+    constructor() {
+        this.player = new Player(this)
+        this.alien = new Alien()
+        this.ticks()
     }
-    
-}
-function updateGame(something) {
-    requestAnimationFrame(updateGame)
-    
-}
-
-updateGame()
-createPlayer()
-
-function createPlayer(){
-    let createPlayer = screen
-    console.log("hi")
-    createPlayer.fillStyle = "#000000"
-    createPlayer.fillRect(screenSize.x/2-15, 350, 15, 15 )
-}
-window.addEventListener("keydown", draw, false);
-function draw(e){
-    switch(e.keyCode) {
-        case 37:
-            // left key pressed
-            break;
-        case 38:
-            // up key pressed
-            break;
-        case 39:
-            // right key pressed
-            break;
-        case 40:
-            // down key pressed
-            break;  
+    draw() {
+        context.clearRect(0, 0, 500, 500)
+        this.player.draw()
+        this.alien.draw()
+        
     }
-    screen.clearRect(0, 0, screenSize.x, screenSize.y) 
+    ticks() {
+        this.update()
+        this.draw()
+        requestAnimationFrame(() => this.ticks())
+    }
+    update(){
+        this.player.update()
+        this.alien.update()
+        
+    }
 }
-draw()
+context.clearRect(0, 0, 500, 500)
+
+class Player {
+    constructor(game) {
+        this.center = {
+            x: screenSize.x / 2 - 10,
+            y: 450
+        }
+        this.playerSize = {
+            x: 20,
+            y: 20
+        }
+        this.keyboarder = new Keyboarder()
+    }
+    draw() {
+        context.fillStyle = "#000000"
+        context.fillRect(this.center.x, this.center.y, this.playerSize.x, this.playerSize.y)
+    }
+    update(){
+        if(this.keyboarder.isDown(Keyboarder.KEYS.LEFT)){
+            this.center.x -= 2
+            if(this.center.x <= 0) this.center.x = 0
+        }
+        if(this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)){
+            this.center.x += 2
+            if(this.center.x >= 490) this.center.x = 490
+        }
+        // if(this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)){
+        //     shootPellett()
+        // }
+        
+    }
+ 
+}
+class Alien {
+    constructor(){
+        this.alienStart = {
+            x:screenSize.x/2-5,
+            y: 20
+        }
+        this.alien = {
+            x:10,
+            y:10
+        }
+
+    }
+    draw() {
+        context.fillStyle = "#000000"
+        context.fillRect(this.alienStart.x, this.alienStart.y, this.alien.x, this.alien.y)
+    }
+    update(){
+        this.alienStart.y += 1
+    }
+}
+
+
+
+
+const alien = new Alien()
+const game = new Game()
