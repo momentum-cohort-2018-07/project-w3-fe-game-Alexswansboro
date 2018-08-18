3b85bb18313edc689f9de15b23474224f3c134aa
Alex Corey <alex.swansboro@gmail.com>
2018-08-17 21:43:25 -0400

    several aliens created

--- script.js
+++ script.js
@@ -7,24 +7,28 @@ const screenSize = {
 class Game {
     constructor() {
         this.player = new Player(this)
-        this.alien = new Alien()
+        this.aliens = [new Alien(), new Alien(), new Alien(), new Alien()] 
         this.ticks()
     }
     draw() {
         context.clearRect(0, 0, 500, 500)
         this.player.draw()
-        this.alien.draw()
-        
+        this.aliens.forEach(function(alien){
+            alien.draw()
+        })
     }
     ticks() {
         this.update()
         this.draw()
         requestAnimationFrame(() => this.ticks())
     }
-    update(){
+    update() {
         this.player.update()
-        this.alien.update()
+        this.aliens.forEach(function(alien){
+            alien.update()
+        })
         
+
     }
 }
 context.clearRect(0, 0, 500, 500)
@@ -45,39 +49,38 @@ class Player {
         context.fillStyle = "#000000"
         context.fillRect(this.center.x, this.center.y, this.playerSize.x, this.playerSize.y)
     }
-    update(){
-        if(this.keyboarder.isDown(Keyboarder.KEYS.LEFT)){
+    update() {
+        if (this.keyboarder.isDown(Keyboarder.KEYS.LEFT)) {
             this.center.x -= 2
-            if(this.center.x <= 0) this.center.x = 0
+            if (this.center.x <= 0) this.center.x = 0
         }
-        if(this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)){
+        if (this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)) {
             this.center.x += 2
-            if(this.center.x >= 490) this.center.x = 490
+            if (this.center.x >= 490) this.center.x = 490
         }
         // if(this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)){
         //     shootPellett()
         // }
-        
+
     }
- 
+
 }
 class Alien {
-    constructor(){
+    constructor() {
         this.alienStart = {
-            x:screenSize.x/2-5,
+            x: screenSize.x / 2 - 5,
             y: 20
         }
         this.alien = {
-            x:10,
-            y:10
+            x: 10,
+            y: 10
         }
-
     }
     draw() {
-        context.fillStyle = "#000000"
+        context.fillStyle = "blue"
         context.fillRect(this.alienStart.x, this.alienStart.y, this.alien.x, this.alien.y)
     }
-    update(){
+    update() {
         this.alienStart.y += 1
     }
 }
@@ -85,5 +88,5 @@ class Alien {
 
 
 
-const alien = new Alien()
-const game = new Game()
+    const alien = new Alien()
+    const game = new Game()
